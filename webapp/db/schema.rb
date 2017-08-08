57303e831706f557059fc195cd3c8f95a7071efd
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-08-08 23:15:12 +0200

    Rename model Train -> Training

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170808205132) do
 
-  create_table "trains", force: :cascade do |t|
+  create_table "trainings", force: :cascade do |t|
     t.string   "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
