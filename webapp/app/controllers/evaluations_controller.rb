4cb8998ffda4e7e50319f6ef30b1bb390318cacd
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-08-08 23:38:22 +0200

    Add form for evaluations with basic action

--- webapp/app/controllers/evaluations_controller.rb
+++ webapp/app/controllers/evaluations_controller.rb
@@ -1,4 +1,15 @@
 class EvaluationsController < ApplicationController
   def index
   end
+
+  def create
+    data = evaluation_params[:data]
+    redirect_to evaluations_path, notice: "Thanks! You've just evaluated #{data}"
+  end
+
+  private
+
+  def evaluation_params
+    params.permit(:data)
+  end
 end
--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "evaluations#index"
   resources :trainings, only: [:index, :new, :create]
-  resources :evaluations, only: :index
+  resources :evaluations, only: [:index, :create]
 end
