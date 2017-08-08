cb1afbcfd240320d84a4a0de13bcd8558d202f5b
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-08-08 23:15:27 +0200

    Add new and create actions to trainings controller

--- webapp/app/controllers/trainings_controller.rb
+++ webapp/app/controllers/trainings_controller.rb
@@ -1,4 +1,23 @@
 class TrainingsController < ApplicationController
   def index
   end
+
+  def new
+    @training = Training.new
+  end
+
+  def create
+    @training = Training.new(training_params)
+    if @training.save
+      redirect_to trainings_path, notice: "Train added successfully"
+    else
+      redirect_to trainings_path, alert: "Error while adding train"
+    end
+  end
+
+  private
+
+  def training_params
+    params.require(:training).permit(:data)
+  end
 end
